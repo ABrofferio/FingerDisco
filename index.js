76a3660501f20bc6a78a58128707c54f6edd682a
Aja Brofferio <aja.brofferio@gmail.com>
2017-05-11 21:49:36 -0700

    adding a discoball and mute button

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 let theBody = document.getElementsByTagName("body")[0];
-let colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+let audio = document.getElementsByTagName("audio")[0];
+let colors = ["red", "orange", "yellow", "green", "blue", "purple", "magenta", "aqua", "white", "black"];
 
 theBody.addEventListener('keydown',(function(){
     theBody.style.backgroundColor = colors[0];
-    let audio = document.getElementsByTagName("audio")[0];
     audio.loop=true;
     audio.play();
     let theColor = colors.shift();
     colors.push(theColor);
+    $('p').hide();
 }));
-document.addEventListener('keyup',(function(){
+theBody.addEventListener('keyup',(function(){
     theBody.style.backgroundColor = colors[0];
+    $('p').hide();
+}));
+$('button').on('click',(function(){
+    audio.pause();
+    $('p').hide();
 }));
